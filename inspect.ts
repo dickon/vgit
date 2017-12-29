6ce009c8b897db5b6bc7a2856917e8c5fa4030c9
Dickon Reed <dickon@cantab.net>
2017-12-29 20:54:01 +0000

    simple web server

--- inspect.ts
+++ inspect.ts
@@ -1,21 +1,29 @@
 import { Repository, Commit } from "nodegit";
 import * as express from "express";
-
+import * as path from "path";
+import opn = require("opn");
 interface StringListMap { [s: string]: string[] };
+
+// see http://thecodebarbarian.com/using-async-await-with-mocha-express-and-mongoose.html
+function wrap(fn) {
+    return function(req, res, next) {
+      // Make sure to `.catch()` any errors and pass them along to the `next()`
+      // middleware in the chain, in this case the error handler.
+      fn(req).then(returnVal => res.send(returnVal)).catch(next);
+    };
+  }
 
 async function recurse(rev: Commit, depth: number, network:StringListMap ) {
     if (depth == 0) return;
     var parents = await rev.getParents(10);
     for (let parent of parents) {
         await recurse(parent, depth - 1, network);
-        console.log("parent "+parent);
         let h = rev.id()+"";
         if (network[h] === undefined) {
             network[h] = [];
         }    
         network[h].push(parent+"");
     }
-
 }
 
 async function read() {
@@ -30,4 +38,11 @@ async function read() {
     }
 }
 
-read();
+let app = express();
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+let server = app.listen(0, () => { 
+    console.log(`listening on ${server.address().port}`);    
+    read();    
+    opn(`http://127.0.0.1:${server.address().port}`);
+});
+
