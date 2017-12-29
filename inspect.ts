1195db5cc8514a3914d370d2cf0b66955d327e7e
Dickon Reed <dickon@cantab.net>
2017-12-29 18:13:31 +0000

    walk a certain depth

--- inspect.ts
+++ inspect.ts
@@ -1,20 +1,27 @@
-import { AsyncTest, Expect, Test, TestCase, TestFixture } from "alsatian";
-import { Repository } from "nodegit";
+import { Repository, Commit } from "nodegit";
 
-@TestFixture("whatever you'd like to call the fixture")
-export class SetOfTests {
-    
-
-    // pass arguments into your test functions to keep your test code from being repetative
-    @TestCase(2, 2, 4)
-    @TestCase(2, 3, 5)
-    @TestCase(3, 3, 6)
-    @AsyncTest("addition tests")
-    public async addTest(firstNumber: number, secondNumber: number, expectedSum: number) {
-        let repo = await Repository.open("../alsatian");
-        let commit = await repo.getBranchCommit("master");
-        let message = commit.message();
-        console.log("message "+message);
-        Expect(firstNumber + secondNumber).toBe(expectedSum);
+async function recurse(rev: Commit, depth: number ) {
+    if (depth == 0) return;
+    console.log("at "+rev+" depth "+depth)
+    var parents = await rev.getParents(10);
+    for (let parent of parents) {
+        console.log(`${rev}->${parent}`);
+        recurse(parent, depth - 1);
     }
 }
+
+async function read() {
+    let repo = await Repository.open("../alsatian");
+    let commit = await repo.getBranchCommit("master");
+    let message =  await commit.message();
+    let parents = await commit.getParents(2);
+    console.log("message "+message);
+    console.log("|parents| = "+parents.length);
+    for (let parent of parents) {
+        console.log("parent "+parent);
+        recurse(parent, 10)
+    }
+    console.log("Done");
+}
+
+read();
